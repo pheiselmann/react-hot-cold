d86f11e61812f22e64be08be0a7720fcff96a8f2
pheiselmann <prhmann@gmail.com>
2017-12-07 19:37:51 -0800

    add intialState and put ADD_GUESS in hotColdReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+import * as actions from '../actions';
+
+const initialState = {
+	guesses: [1, 100, 50, 25],
+	feedback: 'Sorry, that is not right',
+	auralStatus: '',
+	correctAnswer: -1
+};
+
+export const hotColdReducer = (state=initialState, action) => {
+
+	if (action.type === actions.ADD_GUESS) {
+		return Object.assign({}, state, {
+			guesses: [...state.guesses, { action.guess }]
+		});
+	}
+
+	return state;
+
+};
